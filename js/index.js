20b27cb445d6b6f27620e932fc044a80142a3403
Fullchee Zhang <fullchee@gmail.com>
2018-01-01 11:17:31 -0700

    Add pool commands

--- js/index.js
+++ js/index.js
@@ -1,67 +1,128 @@
 "use strict";
 
-const command = document.getElementById("command");
-const description = document.getElementById("description");
-const feedbackButtons = document.getElementById("feedbackButtons");
- 
-const poolCommands = [
-	"Paddles Up: ",
-	"Catch",
-	"Half Stroke",
-	"Second Half"
+let commands;
+
+const backupPoolCommands = [
+   {  
+      "name":"Paddles up",
+      "description":`
+      <ul>
+      	<li>Ready to paddle
+      		<li>Paddle out of the water but you are in your position ready to perform the catch</li>
+      		<li>back straight</li>
+      		<li>bottom arm extended</li>
+      		<li>rotation from your core</li>
+      	</li>
+      </ul>`,
+      "gif":""
+   },
+   {
+   		"name": "Catch",
+   		"description": `
+   		<ul>
+   			<li>Stretch as far as you can.</li>
+   			<li>Imagine spearing a fish in front of you.</li>
+   			<li>Another analogy: Imagine pole vaulting and putting all your weight down on your paddle
+   				<li>makes more sense on the actual boat</li>
+   			</li>
+   		</ul>`,
+   		"gif": ""
+   },
+   {
+   		"name": "Half Stroke",
+   		"description": "The first half of the stroke. Emphasis on the rotation of the stroke. Keep your bottom arm straight. Stop at around the mid thigh.",
+   		"gif": ""
+   },
+	{
+		"name": "Second Half",
+		"description": "The second half of the stroke. Start bending your bottom arm. ",
+		"gif": "",
+	},
+	{
+		"name": "Recovery",
+		"description": "Bring your paddle up to the 'Paddles up' position leading with your top arm.",
+		"gif": "",
+	}
+
 ];
 
+// deep copy of the pool commands
+let poolCommands = JSON.parse(JSON.stringify(backupPoolCommands));
+
 const backupCommands = [  
-   // {  
-   //    "name":"Back it down",
-   //    "description":"make small strokes with your paddle backwards to move the boat backwards/out of the dock",
-   //    "gif":""
-   // },
-   // {  
-   //    "name":"Hold",
-   //    "description":"place your paddle in the water to slow/stop the boat’s momentum (also known as ‘check’ - you may hear this at regattas)",
-   //    "gif":""
-   // },
-   // {  
-   //    "name":"Hold hard",
-   //    "description":"place paddle vertically into water to stop the boat from moving",
-   //    "gif":""
-   // },
-   // {  
-   //    "name":"Feather",
-   //    "description":"run paddle back and forth across water (imagine spreading peanut butter on toast mmmm) to hold the boat steady",
-   //    "gif":""
-   // },
-   // {  
-   //    "name":"Draw",
-   //    "description":"used to straighten up the boat. Paddles placed perpendicularly to the side of the boat and strokes taken towards the boat",
-   //    "gif":""
-   // },
-   // {  
-   //    "name":"Paddles up",
-   //    "description":"ready to paddle - paddle out of the water but you are in your position ready to perform the catch; back straight, bottom arm extended, rotation from your core",
-   //    "gif":""
-   // },
-   // {  
-   //    "name":"Take it away",
-   //    "description":"start paddling",
-   //    "gif":""
-   // },
-   // {  
-   //    "name":"Race ready",
-   //    "description":"Race start p",
-   //    "gif":""
-   // },
-   // {  
-   //    "name":"Ready ready",
-   //    "description":"race start position, paddles buried in the water, weight over the water",
-   //    "gif":""
-   // },
-   // {  
-   //    "name":"Eyes up",
-   //    "description":"call made when the boat isn’t in sync (remember how Ben taught us to watch the people at the front, and not the person sitting in front of you)",
-   //    "gif":""
-   // },
+   {  
+      "name":"Back it down",
+      "description":"make small strokes with your paddle backwards to move the boat backwards/out of the dock",
+      "gif":""
+   },
+   {  
+      "name":"Hold",
+      "description":`
+      <ul>
+      	<li>Place your paddle in the water to slow/stop the boat’s momentum
+      		<li>also known as ‘check’ - you may hear this at regattas</li>
+      	</li>
+      </ul>`,
+      "gif":""
+   },
+   {  
+      "name":"Hold hard",
+      "description":"place paddle vertically into water to stop the boat from moving",
+      "gif":""
+   },
+   {  
+      "name":"Feather",
+      "description":`
+      <ul>
+      	<li>Run paddle back and forth across water
+      		<li>Imagine spreading peanut butter on toast mmmm to hold the boat steady
+      		</li>
+      	</li>
+      </ul>`,
+      "gif":""
+   },
+   {  
+      "name":"Draw",
+      "description":`
+      <ul>
+      	<li>used to straighten up the boat.</li>
+      	<li>Paddles placed perpendicularly to the side of the boat and strokes taken towards the boat</li>
+      </ul>`,
+      "gif":""
+   },
+   {  
+      "name":"Paddles up",
+      "description":`
+      <ul>
+      	<li>Ready to paddle
+      		<li>Paddle out of the water but you are in your position ready to perform the catch</li>
+      		<li>back straight</li>
+      		<li>bottom arm extended</li>
+      		<li>rotation from your core</li>
+      	</li>
+      </ul>`,
+      "gif":""
+   },
+   {  
+      "name":"Take it away",
+      "description":"start paddling",
+      "gif":""
+   },
+   {  
+      "name":"Race ready",
+      "description":"Same as 'Ready Ready'. Race start position, paddles buried in the water, weight over the water",
+      "gif":""
+   },
+   {  
+      "name":"Ready ready",
+      "description":"Same as 'Race ready'. Race start position, paddles buried in the water, weight over the water",
+      "gif":""
+   },
+   {  
+      "name":"Eyes up",
+      "description":"call made when the boat isn’t in sync (remember how Ben taught us to watch the people at the front, and not the person sitting in front of you)",
+      "gif":""
+   },
    {  
       "name":"Let it run",
       "description":"Stop paddling",
@@ -69,7 +130,8 @@ const backupCommands = [
    }
 ];
 
-let commands = JSON.parse(JSON.stringify(backupCommands))
+// deep copy of lake commands
+let lakeCommands = JSON.parse(JSON.stringify(backupCommands))
 
 const seats = {
 	"Seat": "row (eg. seat 4 = 4th row)",
@@ -86,15 +148,17 @@ const seats = {
  * TODO: hide the buttons when not present
  */
 function loadNextCommand() {
+	const feedbackButtons = document.getElementById("feedbackButtons");
+
 	document.getElementById("command").innerHTML = commands[0].name;
-	description.innerHTML = commands[0].description;
+	document.getElementById("description").innerHTML = commands[0].description;
 	document.getElementById("answer").style.display = "none";
 	feedbackButtons.style.display = "none";
 
 	setTimeout(
     	function() {
     		document.getElementById("answer").style.display = "inline";
-      		document.getElementById('response').innerHTML = '<img src="./images/Capture.GIF" size=></img>';
+      		document.getElementById('response').innerHTML = `<img src="${commands[0].gif}"></img>`;
       	feedbackButtons.style.display = "inline";
     	}, 5000);
 
@@ -104,12 +168,15 @@ function loadNextCommand() {
 function learnLakeCommands() {
 	document.getElementById("start").style.display = "none";
 	document.getElementById("question").style.display = "inline";
+	commands = lakeCommands;
 	loadNextCommand();
 }
 
 function learnPoolCommands() {
 	document.getElementById("start").style.display = "none";
-	alert("Sorry, I haven't worked on the pool commands yet");
+	document.getElementById("question").style.display = "inline";
+	commands = poolCommands;
+	loadNextCommand();
 }
 
 /**
